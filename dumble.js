bf4dea33b0d91b11890e4ae044a2f0ab230f1937
Unknown <antrix@cellar>
2007-11-08 04:40:43 +0800

    Delicious integration; genericlinkprovider; lots of fixes and cleanups

--- dumble.js
+++ dumble.js
@@ -1,40 +1,33 @@
-var URLS = new Array("http://video.google.com/videoplay?docid=-5418697513610148116",
+/* var URLS = new Array("http://video.google.com/videoplay?docid=-5418697513610148116",
             "http://www.flickr.com/photos/tnhimmies/1468837710/",
             "http://www.flickr.com/photos/crafty_beaver/1468811180/",
             "http://video.google.com/videoplay?docid=1927769265420370554",
             "http://www.youtube.com/watch?v=MIayUEi_KGo",
             "http://www.youtube.com/watch?v=CTnqFm-wUVQ",
-            "http://www.youtube.com/watch?v=b-GPJw7UttA&watch_response");
+            "http://www.youtube.com/watch?v=b-GPJw7UttA&watch_response"); */
 
 var Providers = new Array();
 
 Providers.push(YoutubeProvider);
 Providers.push(GoogleVideoProvider);
 Providers.push(FlickrProvider);
+Providers.push(GenericLinkProvider); /* Should be last in the array! */
 
-String.prototype.supplant = function (o) {
-    /* http://javascript.crockford.com/remedial.html */
-    return this.replace(/{([^{}]*)}/g,
-        function (a, b) {
-            var r = o[b];
-            return typeof r === 'string' || typeof r === 'number' ? r : a;
-        }
-    );
-};
+$(document).ready(function() {
+    $.getJSON('http://del.icio.us/feeds/json/antrix/linker?count=20&callback=?',
+        function(data) {
+            $.each(data, function(i, item) {
+                $.each(Providers, function() {
+                    var v = this(item.u, item.d, item.n ? item.n : '');
+                    if (v) {
+                        $("#posts").append(
+                            $('<div class="post"></div>\n').hide().prepend(v));
+                        return false;
+                    }
+                });
 
-$(document).ready(
-function() {
+            });
+          $('.post').fadeIn(5000);
+        }); /* getJSON */
 
-    for (i in URLS) {
-        for (p=0; p<Providers.length; p++) {
-            var v = Providers[p](URLS[i]);
-            if (v) {
-                $("#posts").append(
-                    $('<div class="post"></div>').prepend(v).hide().fadeIn(1000));
-                break;
-            }
-        }
-    }
-}
-);
-
+});  /* End $(document).ready() block */
